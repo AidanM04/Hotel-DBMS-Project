8bc835a3995a5b6254cfa4276983ac8dc6fe1d39
AidanM04 <aidanbmaltby@gmail.com>
2024-11-30 16:01:28 -0500

    added admin login functionality and related pages

---
+++ login.py
@@ -0,0 +1,5 @@
+def testlogin(usern, passw):
+    if usern == "htDBMSadmin1" and passw == "adminpasshtdbms6831":
+        return True
+    else:
+        return False
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from roomdata import get_query_result
+from login import testlogin
 from waitress import serve
 
 app = Flask(__name__)
@@ -8,6 +9,30 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/adminlogin')
+def login():
+    return render_template('adminlogin.html')
+
+
+@app.route('/databaseAccess')
+def logintest():
+    usern = request.args.get('username')
+    passw = request.args.get('password')
+
+# Check for empty strings / line with only spaces
+    if not bool(usern.strip()):
+        usern = "NaN"
+
+    if not bool(passw.strip()):
+        passw = "NaN"
+
+    loginresult = testlogin(usern, passw)
+    if loginresult == False:
+        return render_template("loginfailed.html")
+    
+    return render_template("databaseAccess.html")
+
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port = 9000)
