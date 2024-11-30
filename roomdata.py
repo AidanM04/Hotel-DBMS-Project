003505e9c995eb801acc76cb3ae370a6906441e7
AidanM04 <aidanbmaltby@gmail.com>
2024-11-30 14:30:12 -0500

    api commands added

--- roomdata.py
+++ roomdata.py
@@ -5,14 +5,39 @@ import os
 
 load_dotenv()
 
-# all of this is bad placeholder code that I can't really improve on until I know how our actual database is structured and imported / exported to the front end
-def get_query_result(placeholder_query_vars):
-    input_info = f"idk how this works outside of what I've done for a significantly more simple project, it will most definately be unnecessary here"
-    result_json = requests.get(input_info).json()
+# Back end connection functions that take specific key strings, sometimes ids, and sometimes json_info as inputs
+
+
+def get_query_result(get_table):
+    API_URL = f"https://38fd-2607-fb90-398f-8433-adc5-aef7-bb18-b184.ngrok-free.app/"
+    API_URL += get_table
+    result_json = requests.get(API_URL).json()
+
+    return result_json
+
+def add_to_table(add_table, json_arguments):
+    API_URL = f"https://38fd-2607-fb90-398f-8433-adc5-aef7-bb18-b184.ngrok-free.app/"
+    API_URL += add_table
+    result_json = requests.post(API_URL).json()
+
+    return result_json
+
+def update_in_table(update_table, identification, json_arguments):
+    API_URL = f"https://38fd-2607-fb90-398f-8433-adc5-aef7-bb18-b184.ngrok-free.app/"
+    API_URL += f"{update_table}/{identification}"
+    result_json = requests.put(API_URL, json = json_arguments).json()
+
+    return result_json
+
+def delete_from_table(delete_table, identification):
+    API_URL = f"https://38fd-2607-fb90-398f-8433-adc5-aef7-bb18-b184.ngrok-free.app/"
+    API_URL += f"{delete_table}/{identification}"
+    result_json = requests.delete(API_URL).json()
 
     return result_json
 
 
+# testing block
 if __name__ == "__main__":
     print('\n*** Get Database Query results ***\n')
 
