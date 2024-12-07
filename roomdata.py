81e9ef0eddc300693e9c30efc486c08d5f4bbc3c
AidanM04 <aidanbmaltby@gmail.com>
2024-12-06 20:43:09 -0500

    Final commit before turn in -revised v.2

--- roomdata.py
+++ roomdata.py
@@ -2,36 +2,39 @@ from dotenv import load_dotenv
 from pprint import pprint
 from json import loads
 import requests
+import os
 
 
 load_dotenv()
+
+API_url = os.getenv("API_ENDPOINT")
 
 # Back end connection functions that take specific key strings, sometimes ids, and sometimes json_info as inputs
 
 
 def get_query_result(get_table):
-    API_URL = f"https://8f22-2601-c6-d37f-9f10-39c5-d773-d355-3aea.ngrok-free.app/"
+    API_URL = f"{API_url}"
     API_URL += get_table
     result_json = requests.get(API_URL).json()
 
     return result_json
 
 def add_to_table(add_table, json_arguments):
-    API_URL = f"https://8f22-2601-c6-d37f-9f10-39c5-d773-d355-3aea.ngrok-free.app/"
+    API_URL = f"{API_url}"
     API_URL += add_table
     result_json = requests.post(API_URL, json= json_arguments, headers= {"Content-Type": "application/json"}).json()
 
     return result_json
 
 def update_in_table(update_table, identification, json_arguments):
-    API_URL = f"https://8f22-2601-c6-d37f-9f10-39c5-d773-d355-3aea.ngrok-free.app/"
+    API_URL = f"{API_url}"
     API_URL += f"{update_table}/{identification}"
     result_json = requests.put(API_URL, json= json_arguments, headers= {"Content-Type": "application/json"}).json()
 
     return result_json
 
 def delete_from_table(delete_table, identification):
-    API_URL = f"https://8f22-2601-c6-d37f-9f10-39c5-d773-d355-3aea.ngrok-free.app/"
+    API_URL = f"{API_url}"
     API_URL += f"{delete_table}/{identification}"
     result_json = requests.delete(API_URL).json()
 
@@ -44,13 +47,15 @@ if __name__ == "__main__":
 
     function = input("\nPlease enter function name: ")
 
-    input_json = input("\nPlease enter json: ")
+    #input_json = input("\nPlease enter json: ")
 
-    inputjsondict = loads(input_json)
+    #inputjsondict = loads(input_json)
 
-    print(type(inputjsondict))
+    #print(type(inputjsondict))
 
     #result_json = add_to_table(function, input_json)
 
-    #print("\n")
-    #pprint(result_json)
+    result_json= get_query_result(function)
+
+    print("\n")
+    pprint(result_json)
